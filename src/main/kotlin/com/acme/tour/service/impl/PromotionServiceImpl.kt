5f66cb7b8abe6bfc754cb637877696a5c1c21d49
Isaac Henrique <isaachbnn@gmail.com>
2020-11-11 16:45:38 -0300

    remove cache test

--- src/main/kotlin/com/acme/tour/service/impl/PromotionServiceImpl.kt
+++ src/main/kotlin/com/acme/tour/service/impl/PromotionServiceImpl.kt
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class PromotionServiceImpl(private val promotionRepository: PromotionRepository): PromotionService {
-    @Cacheable(cacheManager = "cachemanager", cacheNames = ["promotions_all"])
     override fun getAll(start: Int, size: Int): List<Promotion> =
             this.promotionRepository.findAll(PageRequest.of(start, size)).toList()
 
@@ -19,20 +18,16 @@ class PromotionServiceImpl(private val promotionRepository: PromotionRepository)
     override fun findAllByLocaleContains(locale: String, start: Int, size: Int): List<Promotion> =
             this.promotionRepository.findAllByLocaleContains(locale, PageRequest.of(start, size)).toList()
 
-    @Cacheable(cacheManager = "cachemanager", cacheNames = ["promotion_find_by_id"], key="#id")
     override fun findById(id: Long) = this.promotionRepository.findById(id).orElse(null)
 
-    @CacheEvict(cacheManager = "cachemanager", allEntries = true)
     override fun create(promotion: Promotion) {
         this.promotionRepository.save(promotion)
     }
 
-    @CacheEvict(cacheManager = "cachemanager", cacheNames = ["promotion_find_by_id"], key="#id")
     override fun delete(id: Long) {
         this.promotionRepository.deleteById(id)
     }
 
-    @CacheEvict(cacheManager = "cachemanager", cacheNames = ["promotion_find_by_id"], key="#id")
     override fun update(id: Long, promotion: Promotion) {
         val promotionModel = this.findById(id)
         val promotionUpdate = promotionModel.copy(
